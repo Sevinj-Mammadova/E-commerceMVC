37d90427ef49281596b8db751b5b1e10b4e61b8e
Sevinj-Mammadova <sevinj.mammadova@socardownstream.az>
2025-10-31 15:40:48 +0400

    Add project files.

---
+++ E-commerceMVC/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using E_commerceMVC.Data;
+using E_commerceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerceMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _data;
+
+        public CategoryController(DataContext data)
+        {
+            _data = data;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _data.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //public async Task<IActionResult> GetCategoriesNonPaging()
+        //{
+        //    List<Category> categories = _data.Categories.ToList();
+        //    return View(categories);
+        //}
+    }
+}
---
+++ E-commerceMVC/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using E_commerceMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerceMVC.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
