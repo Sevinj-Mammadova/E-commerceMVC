1f79cab76bb56f5c6a70892e9003b5cf880e483c
Sevinj-Mammadova <sevinj.mammadova@socardownstream.az>
2025-12-01 11:35:41 +0400

    Added Company model and its CRUD operations

---
+++ E-commerce.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using E_commerce.DataAccess.Data;
+using E_commerce.DataAccess.Repository.IRepository;
+using E_commerce.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly DataContext _data;
+
+        public CompanyRepository(DataContext data) : base(data)
+        {
+            _data = data;
+        }
+        public void Update(Company obj)
+        {
+            _data.Companies.Update(obj);
+        }
+    }
+}
+
---
+++ E-commerceMVC/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,85 @@
+﻿using E_commerce.DataAccess.Migrations;
+using E_commerce.DataAccess.Repository.IRepository;
+using E_commerce.Models.Models;
+using E_commerce.Models.ViewModels;
+using E_commerce.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+[Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
+public class CompanyController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public CompanyController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult Index()
+    {
+        List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+        return View(objCompanyList);
+    }
+    public IActionResult Upsert(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return View(new Company());
+        }
+        else
+        {
+            Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+            return View(companyObj);
+        }
+
+    }
+    [HttpPost]
+    public IActionResult Upsert(Company companyObj)
+    {
+        if (ModelState.IsValid)
+        {
+            if (companyObj.Id == 0)
+            {
+                _unitOfWork.Company.Add(companyObj);
+                TempData["success"] = "Company created successfully";
+            }
+            else
+            {
+                _unitOfWork.Company.Update(companyObj);
+                TempData["success"] = "Company updated successfully";
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(companyObj);
+        }
+
+    }
+
+
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+        return Json(new { data = objCompanyList });
+    }
+    [HttpDelete]
+    public IActionResult Delete(int? id)
+    {
+        var productToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+        if (productToBeDeleted == null)
+        {
+            return Json(new { success = false, message = "Error while deleting" });
+        }
+        _unitOfWork.Company.Remove(productToBeDeleted);
+        _unitOfWork.Save();
+        return Json(new { success = true, message = "Delete Successful" });
+    }
+    #endregion
+}
+
