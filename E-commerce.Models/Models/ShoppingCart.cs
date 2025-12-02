4b0aa7dd304f8cd1c34c4f63107bf6f7fa7a0056
Sevinj-Mammadova <sevinj.mammadova@socardownstream.az>
2025-12-02 16:01:22 +0400

    Enabled getting OrderTotal in ShoppingCart

--- E-commerce.Models/Models/ShoppingCart.cs
+++ E-commerce.Models/Models/ShoppingCart.cs
@@ -24,6 +24,8 @@ namespace E_commerce.Models.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+        [NotMapped]
+        public double Price { get; set; }
     }
 
 }
--- E-commerceMVC/Areas/Customer/Controllers/CartController.cs
+++ E-commerceMVC/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_commerce.DataAccess.Repository;
+using E_commerce.DataAccess.Repository.IRepository;
+using E_commerce.Models.Models;
+using E_commerce.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace E_commerceMVC.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ShoppingCartVM shoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+                //OrderTotal =
+
+            };
+            foreach(var cart in shoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBaesOnQuantity(cart);
+                shoppingCartVM.OrderTotal += cart.Price*cart.Count;
+            }
+            return View(shoppingCartVM);
+        }
+        private double GetPriceBaesOnQuantity(ShoppingCart shoppingCart)
+        {
+            if(shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if(shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
         }
     }
 }
