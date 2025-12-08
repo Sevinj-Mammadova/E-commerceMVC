b02a05e7c44016a5ca3150d8cfb22b91fa85d1fc
Sevinj-Mammadova <sevinj.mammadova@socardownstream.az>
2025-12-08 11:33:02 +0400

    Added OrderHeader and OrderDetails models and repositories

---
+++ E-commerce.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using E_commerce.DataAccess.Data;
+using E_commerce.DataAccess.Repository.IRepository;
+using E_commerce.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace E_commerce.DataAccess.Repository
+{ 
+public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+{
+    private DataContext _data;
+    public OrderDetailRepository(DataContext data) : base(data)
+    {
+        _data = data;
+    }
+
+    public void Update(OrderDetail obj)
+    {
+        _data.OrderDetails.Update(obj);
+    }
+}
+}
---
+++ E-commerce.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,26 @@
+﻿using E_commerce.DataAccess.Data;
+using E_commerce.DataAccess.Repository.IRepository;
+using E_commerce.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace E_commerce.DataAccess.Repository
+{ 
+public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+{
+    private DataContext _data;
+    public OrderHeaderRepository(DataContext data) : base(data)
+    {
+        _data = data;
+    }
+
+    public void Update(OrderHeader obj)
+    {
+        _data.OrderHeaders.Update(obj);
+    }
+}
+}
